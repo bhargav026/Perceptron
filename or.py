7a1d69859015e0f04d8202225230b93be9e8ad85
Bhargav Killada <bhargavan@iisc.ac.in>
2023-08-26 11:42:54 +0530

    OR.py added

--- or.py
+++ or.py
@@ -1,24 +1,23 @@
 from utils.all_utils import prepare_data, save_plot
 import pandas as pd
 from utils.model import Perceptron
+def main(data, modelName, plotName, eta, epochs):
+    df_OR = pd.DataFrame(data)
+    X, y = prepare_data(df_OR, "y")
+    model_or = Perceptron(eta=eta, epochs=epochs)
 
-OR = {
-    "X1":[0,0,1,1],
-    "X2":[0,1,0,1],
-    "y":[0,1,1,1]
-}
-df_OR = pd.DataFrame(OR)
+    model_or.fit(X, y)
+    _ = model_or.total_loss()
 
-X, y = prepare_data(df_OR,"y" )
+    model_or.save(filename=modelName, model_dir="model")
+    save_plot(df_OR, model_or, filename=plotName)
 
-ETA = 0.1
-EPOCHS = 10
-
-model_or = Perceptron(eta =ETA, epochs= EPOCHS)
-
-model_or.fit(X,y)
-_ = model_or.total_loss()
-
-model_or.save(filename = "or.model",model_dir = "model")
-
-save_plot(df_OR, model_or, filename='OR.png')
+if __name__ = "__main__":
+    OR = {
+        "X1":[0,0,1,1],
+        "X2":[0,1,0,1],
+        "y":[0,1,1,1]
+    }
+    ETA = 0.1
+    EPOCHS = 10
+    main(data = OR, modelName = "or.model", plotName= "OR.png", eta =ETA, epochs= EPOCHS)
